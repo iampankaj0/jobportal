8644320484d6427c0239e1249f6235ae62de4af6
Pankaj Yadav <91059277+iampankaj0@users.noreply.github.com>
2022-02-21 10:38:43 +0530

    npm

--- src/components/home-components/Jobsection.jsx
+++ src/components/home-components/Jobsection.jsx
@@ -1,41 +1,45 @@
 import React from 'react'
-import { Link } from 'react-router-dom'
+import { Link } from 'react-router-dom';
+import { GrNew } from 'react-icons/gr';
 
-import '../../assets/css/jobsection.css'
+import images from '../../constants/images';
+import '../../assets/css/jobsection.css';
 const jobsection = () => {
     return (
         <>
             <section className="job-section">
-                <h2 className='heading'>Sarkaari Pariksha : sarkaaripariksha.Com Sarkari Naukri Latest Jobs Online Form at Sarkaari Pariksha 2022</h2>
+                <div className="app_wrapper">
+                    <h2 className='heading'> <GrNew /> Sarkaari Pariksha : sarkaaripariksha.Com Sarkari Naukri Latest Jobs Online Form at Sarkaari Pariksha 2022</h2>
+                </div>
                 <div className="app_wrapper">
                     <div className="result">
                         <div className="sec-body">
                             <h2>Result</h2>
                             <div className="links">
                                 <ul>
+                                    <li> <Link to="/">RBI Office Attendant 2020 Final Result</Link> <img src={images.newBadge} alt="newBadge" /> </li>
+                                    <li> <Link to="/">NSUT Various Non Teaching Post Result 2022</Link> <img src={images.newBadge} alt="newBadge" /> </li>
+                                    <li> <Link to="/">NTA UGC NET Result 2022</Link> </li>
+                                    <li> <Link to="/">RSMSSB Stenographer 2018 Phase II Exam Results</Link> </li>
+                                    <li> <Link to="/">DSSSB TGT Social Science Result 2022</Link> </li>
+                                    <li> <Link to="/">UPHESC Assistant Professor Result 2022</Link> </li>
+                                    <li> <Link to="/">IBPS PO XI Main Result with Score Card 2022</Link> </li>
+                                    <li> <Link to="/">BPSC Assistant Conservator Forest 2019 Final Result</Link> </li>
+                                    <li> <Link to="/">IBPS SO XI Main Result 2022</Link> </li>
                                     <li> <Link to="/">MPPSC Pre 2020 Result with Score Card</Link> </li>
                                     <li> <Link to="/">BPSC Civil Judge 2020 Main Result</Link> </li>
-                                    <li> <Link to="/">IBPS PO XI Main Result 2022</Link> </li>
                                     <li> <Link to="/">RPSC Sub Inspector SI 2021 Marks</Link> </li>
-                                    <li> <Link to="/">DSSSB TGT 34/21 Hindi Result</Link> </li>
                                     <li> <Link to="/">CISCE Board 12 & 10 1st Semester Result 2022</Link> </li>
                                     <li> <Link to="/">DFCCIL Various Post 04/2021 Result</Link> </li>
                                     <li> <Link to="/">SSC Upcoming Result 2022</Link> </li>
                                     <li> <Link to="/">Bihar Police BPSSSC Sub Inspector SI Result 2022</Link> </li>
                                     <li> <Link to="/">UPSC Civil Services 2020 Not Qualified Candidate Marks</Link> </li>
                                     <li> <Link to="/">SSC CPO SI 2019 Final Result</Link> </li>
-                                    <li> <Link to="/">DFCCIL Junior Manager Result 2022</Link> </li>
-                                    <li> <Link to="/">MP PEB Group II Sub Group IV 2020 Re Exam Result</Link> </li>
-                                    <li> <Link to="/">VBSPU Backpaper & Improvement Exam Result 2022</Link> </li>
-                                    <li> <Link to="/">UPPSC RO / ARO Pre 2021 Result</Link> </li>
-                                    <li> <Link to="/">MPPSC State Engineering Service 2020 Result</Link> </li>
-                                    <li> <Link to="/">SSC Stenographer 2020 Result with Marks</Link> </li>
-                                    <li> <Link to="/">Haryana HTET 2021 Result</Link> </li>
                                 </ul>
+                                <Link to='/'>
+                                    <button type='button' className='view-morebtn'>View More</button>
+                                </Link>
                             </div>
-                            <Link to='/'>
-                                <button type='button' className='view-morebtn'>View More</button>
-                            </Link>
                         </div>
                     </div>
                     <div className="admit-card">
@@ -43,29 +47,29 @@ const jobsection = () => {
                             <h2>Admit Card</h2>
                             <div className="links">
                                 <ul>
-                                    <li> <Link to="/">MPPSC Pre 2020 Result with Score Card</Link> </li>
-                                    <li> <Link to="/">BPSC Civil Judge 2020 Main Result</Link> </li>
-                                    <li> <Link to="/">IBPS PO XI Main Result 2022</Link> </li>
-                                    <li> <Link to="/">RPSC Sub Inspector SI 2021 Marks</Link> </li>
-                                    <li> <Link to="/">DSSSB TGT 34/21 Hindi Result</Link> </li>
-                                    <li> <Link to="/">CISCE Board 12 & 10 1st Semester Result 2022</Link> </li>
-                                    <li> <Link to="/">DFCCIL Various Post 04/2021 Result</Link> </li>
-                                    <li> <Link to="/">SSC Upcoming Result 2022</Link> </li>
-                                    <li> <Link to="/">Bihar Police BPSSSC Sub Inspector SI Result 2022</Link> </li>
-                                    <li> <Link to="/">UPSC Civil Services 2020 Not Qualified Candidate Marks</Link> </li>
-                                    <li> <Link to="/">SSC CPO SI 2019 Final Result</Link> </li>
-                                    <li> <Link to="/">DFCCIL Junior Manager Result 2022</Link> </li>
-                                    <li> <Link to="/">MP PEB Group II Sub Group IV 2020 Re Exam Result</Link> </li>
-                                    <li> <Link to="/">VBSPU Backpaper & Improvement Exam Result 2022</Link> </li>
-                                    <li> <Link to="/">UPPSC RO / ARO Pre 2021 Result</Link> </li>
-                                    <li> <Link to="/">MPPSC State Engineering Service 2020 Result</Link> </li>
-                                    <li> <Link to="/">SSC Stenographer 2020 Result with Marks</Link> </li>
-                                    <li> <Link to="/">Haryana HTET 2021 Result</Link> </li>
+                                    <li> <Link to="/">RPSC RAS 2021 Mains Admit Card</Link> <img src={images.newBadge} alt="newBadge" /> </li>
+                                    <li> <Link to="/">BPSC Lower Division Clerk LDC Admit Card 2022</Link> <img src={images.newBadge} alt="newBadge" /> </li>
+                                    <li> <Link to="/">DSSSB Exam 20-27 February 2022 Admit Card</Link> <img src={images.newBadge} alt="newBadge" /> </li>
+                                    <li> <Link to="/">RSMSSB Patwari Document Verification Schedule 2022</Link> <img src={images.newBadge} alt="newBadge" /> </li>
+                                    <li> <Link to="/">Delhi District Court Process Server Interview Letter 2022</Link> </li>
+                                    <li> <Link to="/">ICAR IARI Technician Exam Admit Card 2022</Link> </li>
+                                    <li> <Link to="/">IBPS PO XI Interview Letter 2022</Link> </li>
+                                    <li> <Link to="/">NHB Bank Assistant Manager Admit Card 2022</Link> </li>
+                                    <li> <Link to="/">MP High Court AG III, Stenographer Admit Card 2022</Link> </li>
+                                    <li> <Link to="/">CSBC Bihar Police Sepoy Constable PET Exam Date 2022</Link> </li>
+                                    <li> <Link to="/">UPPCL JE Civil Admit Card 2022</Link> </li>
+                                    <li> <Link to="/">UPPCL Assistant Accountant Admit Card 2022</Link> </li>
+                                    <li> <Link to="/">UPPCL ARO 2021 Admit Card</Link> </li>
+                                    <li> <Link to="/">AWES Army School TGT PGT PRT Admit Card 2022</Link> </li>
+                                    <li> <Link to="/">CSBC Bihar Police Prohibition Constable Admit Card 2022</Link> </li>
+                                    <li> <Link to="/">SEBI Assistant Manager Admit Card 2022</Link> </li>
+                                    <li> <Link to="/">DSSSB Exam Date March 2022</Link> </li>
+                                    <li> <Link to="/">UPSC IFS 2021 Main Admit Card</Link> </li>
                                 </ul>
+                                <Link to='/'>
+                                    <button type='button' className='view-morebtn'>View More</button>
+                                </Link>
                             </div>
-                            <Link to='/'>
-                                <button type='button' className='view-morebtn'>View More</button>
-                            </Link>
                         </div>
                     </div>
                     <div className="lst-job">
@@ -73,29 +77,29 @@ const jobsection = () => {
                             <h2>Latest Jobs</h2>
                             <div className="links">
                                 <ul>
-                                    <li> <Link to="/">MPPSC Pre 2020 Result with Score Card</Link> </li>
-                                    <li> <Link to="/">BPSC Civil Judge 2020 Main Result</Link> </li>
-                                    <li> <Link to="/">IBPS PO XI Main Result 2022</Link> </li>
-                                    <li> <Link to="/">RPSC Sub Inspector SI 2021 Marks</Link> </li>
-                                    <li> <Link to="/">DSSSB TGT 34/21 Hindi Result</Link> </li>
-                                    <li> <Link to="/">CISCE Board 12 & 10 1st Semester Result 2022</Link> </li>
-                                    <li> <Link to="/">DFCCIL Various Post 04/2021 Result</Link> </li>
-                                    <li> <Link to="/">SSC Upcoming Result 2022</Link> </li>
-                                    <li> <Link to="/">Bihar Police BPSSSC Sub Inspector SI Result 2022</Link> </li>
-                                    <li> <Link to="/">UPSC Civil Services 2020 Not Qualified Candidate Marks</Link> </li>
-                                    <li> <Link to="/">SSC CPO SI 2019 Final Result</Link> </li>
-                                    <li> <Link to="/">DFCCIL Junior Manager Result 2022</Link> </li>
-                                    <li> <Link to="/">MP PEB Group II Sub Group IV 2020 Re Exam Result</Link> </li>
-                                    <li> <Link to="/">VBSPU Backpaper & Improvement Exam Result 2022</Link> </li>
-                                    <li> <Link to="/">UPPSC RO / ARO Pre 2021 Result</Link> </li>
-                                    <li> <Link to="/">MPPSC State Engineering Service 2020 Result</Link> </li>
-                                    <li> <Link to="/">SSC Stenographer 2020 Result with Marks</Link> </li>
-                                    <li> <Link to="/">Haryana HTET 2021 Result</Link> </li>
+                                    <li> <Link to="/">UPRVUNL Various Post Online Form 2022</Link> <img src={images.newBadge} alt="newBadge" /> </li>
+                                    <li> <Link to="/">Indian Navy Various Officers Entry Online Form 2022</Link> <img src={images.newBadge} alt="newBadge" /> </li>
+                                    <li> <Link to="/">Indian Navy Tradesman Online Form 2022</Link> <img src={images.newBadge} alt="newBadge" /> </li>
+                                    <li> <Link to="/">Coast Guard Assistant Commandant 01/2023 Online Form</Link> </li>
+                                    <li> <Link to="/">UPPSC Assistant Professor 2020 Online Form Re Open</Link> </li>
+                                    <li> <Link to="/">RBI Assistant Online Form 2022</Link> </li>
+                                    <li> <Link to="/">MPPSC Pre 2021 Online Form Re Open</Link> </li>
+                                    <li> <Link to="/">UP NHM CHO Online Form 2022 Extended</Link> </li>
+                                    <li> <Link to="/">Bihar SHSB CHO Online Form 2022</Link> </li>
+                                    <li> <Link to="/">Indian Airforce Apprentice Online Form 2022</Link> </li>
+                                    <li> <Link to="/">East Coast Railway Apprentice Online Form 2022</Link> </li>
+                                    <li> <Link to="/">UPPSC Region Inspector 2020 Main Online Form</Link> </li>
+                                    <li> <Link to="/">MPPSC Pre 2021 Online Form Re Open</Link> </li>
+                                    <li> <Link to="/">UP NHM CHO Online Form 2022 Extended</Link> </li>
+                                    <li> <Link to="/">Bihar SHSB CHO Online Form 2022</Link> </li>
+                                    <li> <Link to="/">Indian Airforce Apprentice Online Form 2022</Link> </li>
+                                    <li> <Link to="/">East Coast Railway Apprentice Online Form 2022</Link> </li>
+                                    <li> <Link to="/">UPPSC Region Inspector 2020 Main Online Form</Link> </li>
                                 </ul>
+                                <Link to='/'>
+                                    <button type='button' className='view-morebtn'>View More</button>
+                                </Link>
                             </div>
-                            <Link to='/'>
-                                <button type='button' className='view-morebtn'>View More</button>
-                            </Link>
                         </div>
                     </div>
                 </div>
@@ -104,4 +108,4 @@ const jobsection = () => {
     )
 }
 
-export default jobsection
+export default jobsection;
