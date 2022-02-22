a50e3b44302f2f846a7968a0d9c0d81b32fbd8d2
Pankaj Yadav <91059277+iampankaj0@users.noreply.github.com>
2022-02-22 09:39:00 +0530

    deploy

--- src/App.js
+++ src/App.js
@@ -3,11 +3,38 @@ import './App.css';
 import Menu from './components/Menu';
 import Topheader from './components/Topheader';
 
+
+import { Route, Switch } from 'react-router-dom';
+
+
+import Home from './components/Home';
+import About from './components/About';
+import Latestjob from './components/Latestjob';
+import Admitcard from './components/Admitcard';
+import Result from './components/Result';
+import Answerkey from './components/Answerkey';
+import Notice from './components/Notice';
+import Contactus from './components/Contactus';
+
+
+
 const App = () => {
   return (
     <>
       <Topheader />
       <Menu />
+
+      <Switch>
+        <Route exact path="/" component={Home} />
+        <Route path="/latestjob" component={Latestjob} />
+        <Route path="/admitcard" component={Admitcard} />
+        <Route path="/result" component={Result} />
+        <Route path="/answerkey" component={Answerkey} />
+        <Route path="/notice" component={Notice} />
+        <Route path="/contact" component={Contactus} />
+        <Route path="/about" component={About} />
+      </Switch>
+
     </>
   )
 }
--- src/index.js
+++ src/index.js
@@ -3,12 +3,10 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 import { BrowserRouter } from 'react-router-dom';
-import Routing from './components/Routing';
 
 ReactDOM.render(
   <BrowserRouter>
     <App />
-    <Routing />
   </BrowserRouter>,
   document.getElementById('root')
 );
